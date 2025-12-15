d15e0dcc4ec57a06823926b21dbc782d803464f0
maxbauerw@gmail.com <maxbauerw@gmail.com>
2025-12-15 13:40:18 -0500

    huh

--- sample-project/src/utils/classnames.ts
+++
@@ -1,8 +0,0 @@
-type ClassValue = string | number | boolean | undefined | null | ClassValue[];
-
-export function cn(...classes: ClassValue[]): string {
-  return classes
-    .flat()
-    .filter((x) => typeof x === 'string' && x.length > 0)
-    .join(' ');
-}
