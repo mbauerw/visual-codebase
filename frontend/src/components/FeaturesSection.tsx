0c8a5370c55b587134753df7c3a76b125010d35a
maxbauerw@gmail.com <maxbauerw@gmail.com>
2026-02-02 00:44:03 -0500

    features changes

--- frontend/src/components/FeaturesSection.tsx
+++ frontend/src/components/FeaturesSection.tsx
@@ -35,30 +35,31 @@ export default function FeaturesSection() {
         </div>
         <div className="grid p-12 md:p-0 md:grid-cols-[2fr_2fr] gap-y-12 lg:gap-x-12 lg:gap-y-12 ">
           {/* Feature 1 */}
-          <div className="bg-white p-8 space-y-4 h-[420px] rounded-3xl shadow-sm border-2 border-gray-400 hover:shadow-xl transition-all duration-300 relative overflow-hidden">
-            {/* <img src='/blueprint.jpeg' alt='ai' className='absolute top-0 left-0 w-full h-full blur-[2px] ' /> */}
+          <div className="bg-white h-[420px] rounded-3xl shadow-sm border border-gray-100 hover:shadow-xl transition-all duration-300 relative overflow-hidden">
+            <img src='/blueprint.jpeg' alt='ai' className='absolute top-0 left-0 w-full h-full blur-[2px] ' />
+            <div className='w-full h-full p-8 z-20 flex flex-col justify-start gap-4 lg:gap-12'>
 
-              <h3 className="text-3xl sm:text-4xl font-semibold text-black z-20 text-center">AI-powered Analysis</h3>
+              <h3 className="text-3xl sm:text-4xl font-semibold text-white z-20 text-center">AI-powered Analysis</h3>
 
               <div className='z-20 h-full overflow-hidden'>
                 {/* Pusher element - pushes the floated image to bottom half */}
-                <div className='float-right h-[50%] sm:h-[20%] w-0' />
+                <div className='float-right h-[50%] sm:h-[30%] w-0' />
                 <img
-                  src="/blueprint.jpeg"
+                  src="/analysis-api.png"
                   alt="analysis api"
-                  className='float-right clear-right max-h-[50%] lg:max-h-[50%] w-[60%] h-[50%] ml-4 rounded-lg object-contain opacity-90'
+                  className='float-right clear-right max-h-[50%] lg:max-h-[80%] w-auto ml-4 rounded-lg object-contain opacity-50'
                 />
-                <p className="text-black text-lg md:text-xl leading-relaxed ">
-                  Codebase Remap generates a full analysis suite for your codebases through the click of a button.
-                  Uncover tech stacks and key modules without having to parse through thousands of lines of code. Efficient up front AST parsing keeps analysis times short and token costs low. 
+                <p className="text-gray-100 text-lg md:text-xl pr-8 leading-relaxed ">
+                  Automatically generate a full analysis suite for your codebases.
+                  Uncover tech stacks and key modules without having to parse through thousands of lines of code.
                 </p>
               </div>
 
- 
+            </div>
           </div>
 
           {/* Feature 2 */}
-          <div className="bg-white  h-[420px] rounded-3xl p-8 space-y-4 shadow-sm border-2 border-gray-400 hover:shadow-xl  transition-all duration-300 relative overflow-hidden">
+          <div className="bg-neutral-100  h-[420px] rounded-3xl p-8 space-y-4 shadow-sm border-4 border-gray-300 hover:shadow-xl  transition-all duration-300 relative overflow-hidden">
             {/* <img src='/visualization.png' alt='vis' className='absolute top-0 left-0 ml-5 mt-10 w-[80%] object-contain opacity-70 z-0' /> */}
 
             <h3 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-900 mb-3 relative z-20 text-center">Interactive Visualization</h3>
@@ -77,8 +78,8 @@ export default function FeaturesSection() {
             </div>
           </div>
 
-          {/* Feature 3 */}
-          <div className="bg-white h-[420px] rounded-3xl px-4 space-y-4 shadow-sm border-2 border-gray-400 hover:shadow-xl transition-all duration-300 relative overflow-hidden">
+          {/* Feature 4 */}
+          <div className="bg-white h-[420px] rounded-3xl px-4 space-y-4 shadow-sm border-2 border-gray-200 hover:shadow-xl transition-all duration-300 relative overflow-hidden">
             <img src='/blackbox.png' alt='AI Assistant' className='absolute top-[120px] right-[30px] h-[65%] rounded-lg !opacity-80' />
 
             <h3 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-black text-center  relative z-20">Support for Nearly All Major Programming Languages</h3>
@@ -102,49 +103,26 @@ export default function FeaturesSection() {
             </div>
           </div>
 
-          {/* Feature 4 */}
-          <div className="bg-white h-[420px] rounded-3xl p-4 space-y-4 shadow-sm border-2 border-gray-400 hover:shadow-xl transition-all duration-300 relative overflow-hidden">
-            <img src='/assistant.png' alt='AI Assistant' className='absolute top-[150px] right-0 h-[65%] rounded-lg !opacity-100' />
+          {/* Feature 3 */}
+          <div className="bg-slate-500 h-[420px] rounded-3xl p-4 space-y-4 shadow-sm border-4 border-gray-600 hover:shadow-xl transition-all duration-300 relative overflow-hidden">
+            <img src='/assistant.png' alt='AI Assistant' className='absolute top-[150px] -right-8 h-[65%] rounded-lg !opacity-80' />
 
-            <h3 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-900 mb- relative z-20 text-center">On-demand AI Assistance</h3>
+            <h3 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-200 mb-3 relative z-20 text-center">On-demand AI Assistance</h3>
             <div className='relative z-20 h-full overflow-hidden'>
               {/* Pusher element - pushes the floated image to bottom half */}
               <div className='float-right h-[20%] w-0' />
               <img
                 src="/assistant.png"
                 alt="AI chatbot"
-                className='float-right clear-right max-h-[55%] w-[60%] rounded-lg object-contain opacity-0'
+                className='float-right clear-right h-full w-[60%] rounded-lg object-contain opacity-0'
               />
-              <p className="text-gray-900 text-lg lg:text-xl leading-relaxed z-20">
-                Our in-app AI assistant leverages the pre-existing codebase analysis as additional context to provide valuable answers whenever you have a question about the code.
+              <p className="text-gray-300 text-lg lg:text-xl leading-relaxed z-20">
+                Our in-app AI assistant leverages the codebase analysis as additional context to provide valuable answers whenever you have a question about the code.
                 In addition, by highlighting text from the analysis suite you can automatically provide specific context for any question you might have.
               </p>
             </div>
           </div>
 
-        {/* Extras */}
-        {/* <div className="bg-white h-[420px] rounded-3xl px-4 space-y-4 shadow-sm border-2 border-gray-200 hover:shadow-xl transition-all duration-300 relative overflow-hidden">
-            <img src='/blackbox.png' alt='AI Assistant' className='absolute top-[120px] right-[30px] h-[65%] rounded-lg !opacity-80' />
-
-            <h3 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-black text-center  relative z-20">Support for Nearly All Major Programming Languages</h3>
-            <div className='relative z-20 h-full overflow-hidden'>
-              <div className='float-right sm:h-[20%] w-0' />
-              <img
-                src="/assistant.png"
-                alt="AI chatbot"
-                className='float-right clear-right max-h-[35%] md:max-h-[45%] w-auto rounded-lg object-contain opacity-0'
-              />
-              <ul className="text-gray-800 pl-4 text-lg lg:text-xl leading-relaxed z-20 list-disc list-inside space-y-2">
-                <li>JavaScript / TypeScript</li>
-                <li>Python</li>
-                <li>Java</li>
-                <li>C#</li>
-                <li>Go</li>
-                <li>Swift</li>
-                <li>Rust</li>
-              </ul>
-            </div>
-          </div> */}
           
 
         </div>
